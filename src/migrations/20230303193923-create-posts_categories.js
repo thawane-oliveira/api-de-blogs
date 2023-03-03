e806f7d8eb8a7f03eca7b2bcc5cd02515ba1eda2
Thawane <thawaneapoliveira@gmail.com>
2023-03-03 16:41:51 -0300

    feat: adiciona requisito 1, criadas migrations para as tabelas users, categories, blog_posts e posts_categories

---
+++ src/migrations/20230303193923-create-posts_categories.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      post_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      category_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  },
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,17 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+    {
+      tableName: 'users',
+      timestamps: false,
+    });
+
+  return User;
+};
+
+module.exports = UserModel;
